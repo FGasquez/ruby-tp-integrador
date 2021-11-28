b0fd9cb09376a5cc12f05b2c3d913c70c5aec6f1
Federico Gasquez <federicogasquez@gmail.com>
2021-11-27 23:54:10 -0300

    Estilos

--- polyconsultorio/app/models/professional.rb
+++ polyconsultorio/app/models/professional.rb
@@ -2,6 +2,7 @@ class Professional < ApplicationRecord
     has_many :appointments, dependent: :destroy
 
     validates :name, uniqueness: { scope: :surname, message: "Professional already exists" }
+    validates :name, :surname, presence: true 
 
     def full_name
         "#{name} #{surname}"
